e2239e75c3a73c7b952d76c689c9228021daebb0
littlepad <littlepad@gmail.com>
2018-01-15 13:09:38 +0900

    wp_head()で自動的に出力される余分なタグを削除

--- public/blog/wp-content/themes/littlepad-2017/functions.php
+++ public/blog/wp-content/themes/littlepad-2017/functions.php
@@ -14,4 +14,16 @@ if ( function_exists('register_sidebar') )
 		'after_title' => '',
 	));
 
+/* 「wp_head();」で自動的に出力される余分なタグを削除 */
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'wp_shortlink_wp_head');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+remove_action('wp_head','wp_oembed_add_discovery_links');
+remove_action('wp_head','rest_output_link_wp_head');
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles' );
+remove_action('admin_print_styles', 'print_emoji_styles');
 ?>
