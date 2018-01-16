1289200813decd36ecf15be2d544f552ed98ccfc
littlepad <littlepad@gmail.com>
2018-01-16 12:09:26 +0900

    comment周りのスタイル調整

--- public/blog/wp-content/themes/littlepad-2017/comments.php
+++ public/blog/wp-content/themes/littlepad-2017/comments.php
@@ -7,11 +7,11 @@ if ( post_password_required() ) : ?>
 <p><?php _e('Enter your password to view comments.'); ?></p>
 <?php return; endif; ?>
 
-<div id="comments">
-<h4><?php comments_number(__('No Comments'), __('1 Comment'), __('% Comments')); ?></h4>
+<div id="comments" class="l-comments">
+<h6 class="l-comments__heading"><?php comments_number(__('No Comments'), __('1 Comment'), __('% Comments')); ?></h6>
 
 <?php if ( have_comments() ) : ?>
-<ol id="commentlist"><?php foreach ($comments as $comment) : ?>
+<ol id="commentlist" class="l-comments__list"><?php foreach ($comments as $comment) : ?>
 <li <?php comment_class(); ?> id="comment-<?php comment_ID() ?>">
 <?php comment_text() ?>
 <p class="m-attribute"><?php comment_type(_x('Comment', 'noun'), __('Trackback'), __('Pingback')); ?> <?php _e('by'); ?> <?php comment_author_link() ?> &#8212; <?php comment_date() ?> <a href="#comment-<?php comment_ID() ?>"><?php comment_time() ?></a> <?php edit_comment_link(__("Edit This"), ' |'); ?></p>
@@ -32,7 +32,7 @@ if ( post_password_required() ) : ?>
 
 <?php if ( comments_open() ) : ?>
 <div id="postcomment">
-<h4><?php _e('Leave a comment'); ?></h4>
+<h6 class="l-comments__heading"><?php _e('Leave a comment'); ?></h6>
 
 <?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
 <p><?php printf(__('You must be <a href="%s">logged in</a> to post a comment.'), wp_login_url( get_permalink() ) );?></p>
@@ -57,7 +57,7 @@ if ( post_password_required() ) : ?>
 
 <?php endif; ?>
 
-<p><textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea></p>
+<p><textarea name="comment" id="comment" cols="30" rows="5" tabindex="4"></textarea></p>
 
 <p><input name="submit" type="submit" id="submit" tabindex="5" value="<?php esc_attr_e('Submit Comment'); ?>" />
 <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
--- public/blog/wp-content/themes/littlepad-2017/index.php
+++ public/blog/wp-content/themes/littlepad-2017/index.php
@@ -7,39 +7,38 @@ get_header();
 ?>
 
 <div class="l-article">
-<main>
-<article>
+  <main>
+    <article>
 
-<?php if ( is_category() ) { ?>
-     <p><strong>“<?php single_cat_title() ?>”</strong> のアーカイブ</p>
-<?php } elseif ( is_month() ) { ?>
-     <p><strong><?php the_time('Y'); ?>年<?php the_time('n'); ?>月</strong> のアーカイブ</p>
-<?php } ?>
+      <?php if ( is_category() ) { ?>
+        <p><strong>“<?php single_cat_title() ?>”</strong> のアーカイブ</p>
+      <?php } elseif ( is_month() ) { ?>
+        <p><strong><?php the_time('Y'); ?>年<?php the_time('n'); ?>月</strong> のアーカイブ</p>
+      <?php } ?>
 
+      <?php if ( is_single() ) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <p class="m-attribute"><?php the_date(); ?> <?php the_time() ?> <?php the_category(',') ?> &#8212; <?php the_tags(__('Tags: '), ', ', ' &#8212; '); ?> <?php the_author() ?> <?php edit_post_link(__('Edit This')); ?></p>
+        <div class="entryContent">
+          <?php the_content(__('[ → 続きを読む ]')); ?>
+        </div>
+        <?php comments_template(); // Get wp-comments.php template ?>
+        <p class="feedback"><?php wp_link_pages(); ?><?php comments_popup_link(__('Comments (0)'), __('Comments (1)'), __('Comments (%)')); ?></p>
+      <?php elseif (have_posts()) : ?>
+        <div class="m-list">
+        <ul>
+        <?php while (have_posts()) : the_post(); ?>
+          <li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></li>
+        <?php endwhile; ?>
+        </ul>
+        </div>
+      <?php else: ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+      <?php endif; ?>
 
-<?php if ( is_single() ) : the_post(); ?>
-  <h1><?php the_title(); ?></h1>
-  <p class="m-attribute"><?php the_date(); ?> <?php the_time() ?> <?php the_category(',') ?> &#8212; <?php the_tags(__('Tags: '), ', ', ' &#8212; '); ?> <?php the_author() ?> <?php edit_post_link(__('Edit This')); ?></p>
-  <div class="entryContent">
-    <?php the_content(__('[ → 続きを読む ]')); ?>
-    <?php comments_template(); // Get wp-comments.php template ?>
-    <p class="feedback"><?php wp_link_pages(); ?><?php comments_popup_link(__('Comments (0)'), __('Comments (1)'), __('Comments (%)')); ?></p>
-  </div>
-<?php elseif (have_posts()) : ?>
-  <div class="m-list">
-  <ul>
-  <?php while (have_posts()) : the_post(); ?>
-    <li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></li>
-  <?php endwhile; ?>
-  </ul>
-  </div>
-<?php else: ?>
-  <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
-
-<p class="navLink"><?php posts_nav_link(' | ', '&laquo; Newer Posts', 'Older Posts &raquo;'); ?></p>
-</article>
-</main>
+      <p class="navLink"><?php posts_nav_link(' | ', '&laquo; Newer Posts', 'Older Posts &raquo;'); ?></p>
+    </article>
+  </main>
 </div>
 
 <?php get_footer(); ?>
