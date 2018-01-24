389e943104f4a85a25d1cf37c3e7b2c76b5cea33
littlepad <littlepad@gmail.com>
2018-01-24 22:37:37 +0900

    不要なjs削除

--- public/blog/wp-content/themes/littlepad-2017/js/common.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    if (! $.browser.safari) {
-        $('p.pageTop a').click(function() {
-            $(this).blur();
-            $('html,body').animate({ scrollTop: 0 }, 1000 );
-            return false;
-        });
-    }
-});
